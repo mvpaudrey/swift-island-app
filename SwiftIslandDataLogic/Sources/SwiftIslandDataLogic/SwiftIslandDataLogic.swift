b821ece2647ae0eede09807da5a7b0d735bfc1b0
Audrey Zebaze <audrey@cheulah.com>
2024-08-24 14:55:07 +0200

    [Dependencies] Remove Firebase for workshop purposes

--- SwiftIslandDataLogic/Sources/SwiftIslandDataLogic/SwiftIslandDataLogic.swift
+++ SwiftIslandDataLogic/Sources/SwiftIslandDataLogic/SwiftIslandDataLogic.swift
@@ -1,7 +1,5 @@
 import SwiftUI
 import os.log
-import Firebase
-import FirebaseFirestore
 
 /// This is the protocol definition for ``SwiftIslandDataLogic``, which can be used for dependency injection
 public protocol DataLogic {
@@ -73,129 +71,95 @@ public class SwiftIslandDataLogic: DataLogic, ObservableObject {
     required public init() { }
 
     /// Configures SwiftIslandDataLogic's dependencies. Required to be run at launch.
-    public static func configure() {
-        FirebaseApp.configure()
-    }
-
-    private let logger = Logger(
-        subsystem: Bundle.main.bundleIdentifier!,
-        category: String(describing: SwiftIslandDataLogic.self)
-    )
-
-    /// Fetches all the stored locations
-    /// - Returns: Array of `Location`
-    public func fetchLocations() async -> [Location] {
-        let request = AllLocationsRequest()
-        return await fetchFromFirebase(forRequest: request)
-    }
-
-    /// Fetches all the mentors from Firebase
-    /// - Returns: Array of `Mentor`
-    public func fetchMentors() async -> [Mentor] {
-        let request = AllMentorsRequest()
-        return await fetchFromFirebase(forRequest: request).sorted(by: { $0.order < $1.order })
-    }
-
-    /// Fetches all the pages from Firebase and stores
-    /// - Returns: Array of `Page`
-    public func fetchPages() async -> [Page] {
-        let request = AllPagesRequest()
-        return await fetchFromFirebase(forRequest: request)
-    }
-
-    /// Fetches all the activities available.
-    ///
-    /// Each activity is seperate from an event; an activity is unique, such as "VisionOS Workshop", but it can happen multiple times.
-    /// - Returns: Array of `Activity`
-    @discardableResult
-    public func fetchActivities() async -> [Activity] {
-        let request = AllActivitiesRequest()
-
-        let activities = await fetchFromFirebase(forRequest: request)
-        self.activities = activities
-        return activities
-    }
-
-    /// Fetches the db events from firebase and converts them to a `Event`
-    /// - Returns: Array of `Event`
-    public func fetchEvents() async -> [Event] {
-        if activities.isEmpty {
-            await fetchActivities()
+        public static func configure() {
         }
 
-        let request = AllEventsRequest()
-        let dbEvents = await fetchFromFirebase(forRequest: request)
+        private let logger = Logger(
+            subsystem: Bundle.main.bundleIdentifier!,
+            category: String(describing: SwiftIslandDataLogic.self)
+        )
 
-        let events: [Event] = dbEvents.compactMap { dbEvent in
-            guard let activity = activities.first(where: { $0.id == dbEvent.activityId }) else { return nil }
-            return Event(dbEvent: dbEvent, activity: activity)
-        }.sorted(by: { $0.startDate < $1.startDate })
-
-        return events
-    }
-
-    /// Fetches the default setup of the packing items available on Firebase. Should only be fetched once per instance
-    /// - Returns: Array of `PackingItem` from firebase
-    public func fetchPackingListItemsFromFirebase() async -> [PackingItem] {
-        let request = AllPackingListItems()
-        return await fetchFromFirebase(forRequest: request).sorted(by: { $0.order < $1.order })
-    }
-
-    public func fetchFAQItems() async -> [FAQItem] {
-        let request = AllFAQRequest()
-        return await fetchFromFirebase(forRequest: request)
-    }
-
-    public func fetchTicket(slug: String, from checkinList: String) async throws -> Ticket {
-        guard let url = URL(string: "https://checkin.tito.io/checkin_lists/\(checkinList)/tickets/\(slug)") else {
-            throw DataLogicError.incorrectSlug
+        /// Fetches all the stored locations
+        /// - Returns: Array of `Location`
+        public func fetchLocations() async -> [Location] {
+            [Location.forPreview()]
         }
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(dateFormatter)
-        return try await fetchModel(Ticket.self, from: url, decoder: decoder)
-    }
 
-    public func fetchAnswers(for checkinList: String) async throws -> [Answer] {
-        guard let url = URL(string: "https://checkin.tito.io/checkin_lists/\(checkinList)/answers") else {
-            throw DataLogicError.incorrectSlug
+        /// Fetches all the mentors from Firebase
+        /// - Returns: Array of `Mentor`
+        public func fetchMentors() async -> [Mentor] {
+            Mentor.forWorkshop()
         }
-        return try await fetchModel(Array<Answer>.self, from: url)
-    }
 
-    public func fetchSponsors() async throws -> Sponsors {
-        let url = URL(string: "https://swiftisland.nl/api/sponsors.json")!
-        return try await fetchModel(Sponsors.self, from: url)
-    }
-
-    public func fetchAnswers(for tickets: [Ticket], in checkinList: String) async throws -> [Int: [Answer]] {
-        let allAnswers = try await fetchAnswers(for: checkinList)
-        var result: [Int: [Answer]] = [:]
-        for ticket in tickets {
-            result[ticket.id] = allAnswers.filter({ $0.ticketId == ticket.id })
+        /// Fetches all the pages from Firebase and stores
+        /// - Returns: Array of `Page`
+        public func fetchPages() async -> [Page] {
+            [Page.forPreview()]
         }
-        return result
-    }
 
-    public func fetchPuzzles() async -> [Puzzle] {
-        let request = AllPuzzlesRequest()
-        return await fetchFromFirebase(forRequest: request).sorted(by: { $0.order < $1.order })
-    }
+        /// Fetches all the activities available.
+        ///
+        /// Each activity is seperate from an event; an activity is unique, such as "VisionOS Workshop", but it can happen multiple times.
+        /// - Returns: Array of `Activity`
+        @discardableResult
+        public func fetchActivities() async -> [Activity] {
+            [Activity.forPreview()]
+        }
+
+        /// Fetches the db events from firebase and converts them to a `Event`
+        /// - Returns: Array of `Event`
+        public func fetchEvents() async -> [Event] {
+            [Event.forPreview()]
+        }
+
+        /// Fetches the default setup of the packing items available on Firebase. Should only be fetched once per instance
+        /// - Returns: Array of `PackingItem` from firebase
+        public func fetchPackingListItemsFromFirebase() async -> [PackingItem] {
+            [PackingItem.forPreview()]
+        }
+
+        public func fetchFAQItems() async -> [FAQItem] {
+            [FAQItem.forPreview()]
+        }
+
+        public func fetchTicket(slug: String, from checkinList: String) async throws -> Ticket {
+            guard let url = URL(string: "https://checkin.tito.io/checkin_lists/\(checkinList)/tickets/\(slug)") else {
+                throw DataLogicError.incorrectSlug
+            }
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(dateFormatter)
+            return try await fetchModel(Ticket.self, from: url, decoder: decoder)
+        }
+
+        public func fetchAnswers(for checkinList: String) async throws -> [Answer] {
+            guard let url = URL(string: "https://checkin.tito.io/checkin_lists/\(checkinList)/answers") else {
+                throw DataLogicError.incorrectSlug
+            }
+            return try await fetchModel(Array<Answer>.self, from: url)
+        }
+
+        public func fetchSponsors() async throws -> Sponsors {
+            let url = URL(string: "https://swiftisland.nl/api/sponsors.json")!
+            return try await fetchModel(Sponsors.self, from: url)
+        }
+
+        public func fetchAnswers(for tickets: [Ticket], in checkinList: String) async throws -> [Int: [Answer]] {
+            let allAnswers = try await fetchAnswers(for: checkinList)
+            var result: [Int: [Answer]] = [:]
+            for ticket in tickets {
+                result[ticket.id] = allAnswers.filter({ $0.ticketId == ticket.id })
+            }
+            return result
+        }
+
+        public func fetchPuzzles() async -> [Puzzle] {
+            [Puzzle.forPreview()]
+        }
 }
 
 private extension SwiftIslandDataLogic {
-    /// Performs the fetch on Firebase with a logger if an issue arrises
-    /// - Parameter request: The request to preform
-    /// - Returns: The output
-    func fetchFromFirebase<R: Request>(forRequest request: R) async -> [R.Output] {
-        do {
-            return try await Firestore.get(request: request)
-        } catch {
-            logger.error("Error getting documents for request with path \(request.path): \(error, privacy: .public)")
-            return []
-        }
-    }
 
     func fetchModel<M: Decodable>(_ model: M.Type, from url: URL, decoder: JSONDecoder = JSONDecoder()) async throws -> M {
         var urlRequest = URLRequest(url: url)
